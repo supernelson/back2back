e669b14b9e139d6ad58fe002162e06644e38765d
parthahirgit config --global user.email parthahir@umail.ucsb.edugit config --global user.name “parthahir <parthahir@umail.ucsb.edu>
2016-09-05 12:13:32 -0700

    Add images to test combined methods of image recognition and geolocation

--- iOS/AugmentedRealityApplication/ViewController.m
+++ iOS/AugmentedRealityApplication/ViewController.m
@@ -53,7 +53,7 @@
         self.architectView.debugDelegate = self;
         
         /* Use the -setLicenseKey method to unlock all Wikitude SDK features that you bought with your license. */
-        [self.architectView setLicenseKey:@"FKH3vTh9q7WQKdjUf46kohX1YybXKyKfTp1clUat2xMEGFgkokF7ERctrdc3xp4uzWtDdU7sGsoFindh4B+OG1adPDMyRTAMazq6It8Z/Em9XxWyJ6ESPAXsFiQQ4JO+J0KWjUGdMtqJ0Zbzt0RZ8D0zTH5TgV1YZWIelJS1Nx5TYWx0ZWRfX/h/24PrljuAb/83KrbI1YFF1FKDSLpuOqrUhTLwO+yFfjZWV798OMY2dIlf6KeQ9J/Nr3jRBa1sWivVfhKxV8oy2aBB0ZVMtpDYuN3txvOdxSVLk4OCpnPg0ux7QUc0nocY0y6fMDy9NsSnyG4kSG2hEaF8ewQ3bCbDkWRTvSTGtrhFQXIt8cDO+IUxbE688swNLMPnQYcONVxWWiVRtFaKGICuR6szXjIgbUuJcw0APybDc/eQO2aW9Pf0UH/AG9OfocD2Ey5UomENPPOKkoo3Kw3BFNz4D19+LS23Ui0gZYu7LxdRyXGZEzZv6YBz8U6pVYsSs+x04hI9ervr7zSWJPy276o9n0AZWFGh/rifjZc5CS/zPq41ulTaT/6trR19aVDBZB00VtrIj3LboUPoFRYSGPRhJFG/ot7mDYTutwF1rp0BQGDUbXgMaKHisUV51fXk8hpt0IrQOkAP1y2y66w2cbMgn/IkBik6jCLhCQUiks4bF5Y="
+        [self.architectView setLicenseKey:@"V//r14dg1mAnH9OQAfMGp/nW0BBshBG0/objjZ2km3t2/CBL+moek+n6CDMq7NJAO1zckS2GebKaXHmphQHpBoJl05cveeuCgHCJnGvpdg2dqysFN3t59/UtbgQe+NkK5gc/7jwcgyimhXa3pdxHdBBgl3xheWH6cMxEEwiCFkZTYWx0ZWRfX6u6RqwtvbO5gN89auZCmpxUXeMTKITZaE1dE3X8kZufiyLCsYpWc6h5G2oPcnx5o7K2k4h/CJwE22qyvJWBcwLq3jbVwYfbuHHptJXsiEsnZuZadLTesZeUR/fIyDp5z1jTjALGulmtK943qyP8QORgzvZcQJan64IEudzhhhVIo1CyKm9lPv2gyK9YQbDZwqvJz46/9ttsSJp1n2lCN79Qp0MTgwvNLDCT4bAJ+PIkGhcTKHi1ukRftpkUsznJAQf1jsTEZg5I6nIVHWN3Dn03vE6cU/R4PINstd5A5qjDWUNNtNahpshCe86njLBc6zYRwWzBHJCqFyhBJHo4APLBLzfIUxccULk8KVgCvRd63MvyN3Q5TeX3clGRwwm05v1KZuQ0JIAvN51TthvnzXXsTY17gn2pTfaZiEu/CjoP8K71PDKQecercYiFqTH0AkcJDUz7V5h7kYb2ubszCYI4+yZo5vvHibaM6A0GTkcdlqD4aTRis5E="
 ];
         
         /* The Architect World can be loaded independently from the WTArchitectView rendering.
